c1f159caf9e378fb2a63cae44d32230b806f919c
Daniel Arroyo <darroylat@gmail.com>
2017-06-22 11:53:53 -0400

    Probando

--- webcliente/index.php
+++ webcliente/index.php
@@ -55,14 +55,13 @@
     <div class="row">
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                <button class="triangulo_top_left"></button>
+                <div class="triangulo_top_left"></div>
                 <img class="img-rounded" src="images/montana.jpg" alt="...">
                 <div class="caption">
                     <h4>Thumbnail label h4</h4>
                     <p><?php echo substr($mensaje, 0, 20).'...'; ?></p>
                     <p style="text-align: right;"><a href="#" class="btn btn-primary" role="button">Ver más</a></p>
                 </div>
-                <button class="triangulo-equilatero-bottom-left"></button>
             </div>
         </div>
         <div class="col-sm-6 col-md-4">
